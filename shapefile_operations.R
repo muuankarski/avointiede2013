6b83bd0b2bddd27c5f6ba6fed2a5d3c9e50cec2a
Markus Kainu <markuskainu@gmail.com>
2013-10-09 18:55:37 +0300

    Demon läpikäyminen - analyysit suoraviivaisemmiksi - fonttien koot - maan rajat, yms

--- shapefile_operations.R
+++ shapefile_operations.R
@@ -1,11 +1,10 @@
 setwd("~/workspace/courses/own_lectures_courses/openroadshow2013_tku")
-download.file("http://gadm.org/data/rda/FIN_adm1.RData", 
-              destfile = "gadm.RData")
-load("gadm.RData")
+download.file("http://gadm.org/data/rda/FIN_adm0.RData", 
+              destfile = "data/gadm.RData")
 library(ggplot2)
 library(rgeos)
 # Ja käsitellään tavanomaiseksi data.frameksi
 gadm@data$id <- rownames(gadm@data)
 gadm.points <- fortify(gadm, region = "id")
 gadm.df <- merge(gadm.points, gadm@data, by = "id")
-save(gadm.df, file="gadm.df.RData")
+save(gadm.df, file="data/gadm.df.RData")
